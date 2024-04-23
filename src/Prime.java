f8c98db25ce2afc4042fd1773f59e02857e2cb89
Gadde Eswar <gaddeeswar07@gmail.com>
2024-04-23 23:31:32 +0530

    Update Prime.java

--- src/Prime.java
+++ src/Prime.java
@@ -1,19 +1,19 @@
 public class Prime {
     public static void main(String[] args) {
         int num = 30;
-       for(int i=num; i>=1;i--){
-           int count=0;
-           for(int j=1; j<=7;j++){
-               if(i%j==0){
-                   count++;
-               }
-           }
+        for (int i = num; i >= 1; i--) {
+            int count = 0;
+            for (int j = 1; j <= 7; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
 
-           if(count==1 || i == 2 ||i== 3 ||i== 5 ||i== 7) {
-               System.out.println(i);
-         
+            if (count == 1 || i == 2 || i == 3 || i == 5 || i == 7) {
+                System.out.println(i);
 
-           }
-       }
-    
+
+            }
+        }
+    }
 }
